37e8e8a681b1f5ac5ec38c8cc544e290775c9b0c
Thomas Delecroix <thomas.delecroix@tetragg.com>
2025-01-07 23:38:34 +0100

    2015-d1-p1(feat): add read loop/charm close and print (with libc)

--- 2015/1.asm
+++ 2015/1.asm
@@ -1,23 +1,74 @@
-global _start
+section .bss
 
-section .data
+tmp_buffer: resb 0x400
 
-input_file db "1.input", 0
+section .rodata
+
+input_file  db  "1.input", 0
+print_int   db  "%d", 10, 0
 
 section .text
 
-_start:
-  mov rax,  0x2
-  lea rdi,  input_file
-  mov rsi,  0x0
-  mov rdx,  0x0
-  syscall
-  cmp rax, $1
-  mov rdi,  rax
-  js  exit
+extern printf
+global main
+
+main:
+open:
+    mov     rax, 0x2
+    lea     rdi, input_file
+    xor     rsi, rsi
+    xor     rdx, rdx
+    syscall
+    test    rax, rax
+    mov     rdi, rax
+    js      exit
+; rbx = fd
+    mov     rbx, rax
+; r12 = res
+    xor     r12, r12
+
+read_loop:
+    xor     rax, rax
+    mov     rdi, rbx
+    lea     rsi, tmp_buffer
+    mov     rdx, 0x400
+    syscall
+    test    rax, rax
+    jz      close
+    mov     rdi, rax
+    js      exit
+; rcx = counter
+    xor     rcx, rcx
+read_char:
+    cmp     byte[tmp_buffer + rcx], '('
+    je      read_incr
+    cmp     byte[tmp_buffer + rcx], ')'
+    je      read_dec
+    jmp     read_end
+read_dec:
+    dec     r12
+    jmp     read_end
+read_incr:
+    inc     r12
+read_end:
+    inc     rcx
+    cmp     rax,  rcx
+    je      read_loop
+    jmp     read_char
+
+close:
+    mov     rax, 0x3
+    mov     rdi, rbx
+    syscall
+
+print:
+    xor     rax, rax
+    mov     rsi, r12
+    mov     rdi, print_int
+    call    printf
 
 success:
-  mov rdi,  0x0
+    mov     rdi, 0x0
 exit:
-  mov rax,  0x3c
-  syscall
+    mov     rax, 0x3c
+    syscall
